36ed852c542ec8c370d06ba5762f797f42db080e
Seok Min <gameplay3542@icloud.com>
2018-06-13 20:57:47 +0900

    UPDATE : Deleted Some Errors

--- convertImage/py_getImage_info.py
+++ convertImage/py_getImage_info.py
@@ -1,7 +1,23 @@
 from PIL import Image
-import py_convert_image
 
+#   Converted File Name
+converted_file_name = 'converted_image/python.jpg'
 
-# Dir Name
-dir_converted = 'converted_image/'
+#   Dir Name
+#dir_converted = '../convertImage/converted_image/'
 
+#   Open Image
+converted_image = Image.open(converted_file_name)
+print(converted_image)
+
+#   Print Image Size
+print(converted_image.size)
+
+#   Get Pixel Code
+cnt = 0
+
+for i in range(1, 33):
+    for j in range(1, 33):
+        cnt = cnt + 1
+        print(cnt, ':', i, ',', j)
+        print(converted_image.getpixel((i-1, j-1)))
