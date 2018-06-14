972136a28202a51496e8f450909d2587e805d048
Seok Min <gameplay3542@icloud.com>
2018-06-14 12:26:42 +0900

    UPDATE : Change RGB(0~255) Code to Matrix Color333(0~7)

--- convertImage/py_getImage_info.py
+++ convertImage/py_getImage_info.py
@@ -16,11 +16,95 @@ print(converted_image)
 #   Print Image Size
 print(converted_image.size)
 
-#   Get Pixel Code
+#   Get Pixel Code & Save RGB(tuple) in List(1024)
 cnt = 0
+cnt1 = 0
 
 for i in range(1, 33):
     for j in range(1, 33):
-        rgb.append(converted_image.getpixel((i-1,j-1)))
+        rgb.append(list(converted_image.getpixel((i-1,j-1))))
+#        print(rgb[cnt])
+
+
+        if rgb[cnt][0] <= 31:
+            rgb[cnt][0] = 0
+
+        elif rgb[cnt][cnt1] >= 32 and rgb[cnt][0] <= 63:
+            rgb[cnt][0] = 1
+
+        elif rgb[cnt][0] >= 64 and rgb[cnt][0] <= 95:
+            rgb[cnt][0] = 2
+
+        elif rgb[cnt][0] >= 96 and rgb[cnt][0] <= 127:
+            rgb[cnt][0] = 3
+
+        elif rgb[cnt][0] >= 128 and rgb[cnt][0] <= 159:
+            rgb[cnt][0] = 4
+
+        elif rgb[cnt][0] >= 160 and rgb[cnt][0] <= 191:
+            rgb[cnt][0] = 5
+
+        elif rgb[cnt][0] >= 192 and rgb[cnt][0] <= 223:
+            rgb[cnt][0] = 6
+
+        elif rgb[cnt][0] >= 224 and rgb[cnt][0] <= 255:
+            rgb[cnt][0] = 7
+
+
+##############################################################
+        if rgb[cnt][1] <= 31:
+            rgb[cnt][1] = 0
+
+        elif rgb[cnt][1] >= 32 and rgb[cnt][1] <= 63:
+            rgb[cnt][1] = 1
+
+        elif rgb[cnt][1] >= 64 and rgb[cnt][1] <= 95:
+            rgb[cnt][1] = 2
+
+        elif rgb[cnt][1] >= 96 and rgb[cnt][1] <= 127:
+            rgb[cnt][1] = 3
+
+        elif rgb[cnt][1] >= 128 and rgb[cnt][1] <= 159:
+            rgb[cnt][1] = 4
+
+        elif rgb[cnt][1] >= 160 and rgb[cnt][1] <= 191:
+            rgb[cnt][1] = 5
+
+        elif rgb[cnt][1] >= 192 and rgb[cnt][1] <= 223:
+            rgb[cnt][1] = 6
+
+        elif rgb[cnt][1] >= 224 and rgb[cnt][1] <= 255:
+            rgb[cnt][1] = 7
+
+
+##############################################################
+        if rgb[cnt][2] <= 31:
+            rgb[cnt][2] = 0
+
+        elif rgb[cnt][2] >= 32 and rgb[cnt][2] <= 63:
+            rgb[cnt][2] = 1
+
+        elif rgb[cnt][2] >= 64 and rgb[cnt][2] <= 95:
+            rgb[cnt][2] = 2
+
+        elif rgb[cnt][2] >= 96 and rgb[cnt][2] <= 127:
+            rgb[cnt][2] = 3
+
+        elif rgb[cnt][2] >= 128 and rgb[cnt][2] <= 159:
+            rgb[cnt][2] = 4
+
+        elif rgb[cnt][2] >= 160 and rgb[cnt][2] <= 191:
+            rgb[cnt][2] = 5
+
+        elif rgb[cnt][2] >= 192 and rgb[cnt][2] <= 223:
+            rgb[cnt][2] = 6
+
+        elif rgb[cnt][2] >= 224 and rgb[cnt][2] <= 255:
+            rgb[cnt][2] = 7
+
+
         print(rgb[cnt])
+
         cnt = cnt + 1
+
+print(rgb[1023][0])
