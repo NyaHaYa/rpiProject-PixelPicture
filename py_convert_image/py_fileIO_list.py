5e38edd183f4fbc0dcaa9ac39a97779ac1ec20a3
Seok Min <gameplay3542@icloud.com>
2018-07-04 20:20:44 +0900

    ADD : Add FileIO - Export RGB Lists to txt file

--- py_convert_image/py_fileIO_list.py
+++ py_convert_image/py_fileIO_list.py
@@ -1,3 +1,10 @@
-def _fileIO_list(fileName, data):
-    f = open(fileName + ".txt", 'w')
-    
+def _fileIO_list(_fileName, data):
+    f = open("/Users/gameplay/Desktop/rpiProject-PixelPicture/list/" + _fileName + ".txt", 'w')
+
+    idata = "%s" % data
+
+    replace1_data = idata.replace("[","")
+    replace2_data = replace1_data.replace("]","")
+
+    f.write(replace2_data)
+    f.close()
