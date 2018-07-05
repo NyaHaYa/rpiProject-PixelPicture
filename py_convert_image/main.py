abba8382b2c2e8d011ceaa33395bdad09f26f2dc
NyaHaYa <gameplay3542@icloud.com>
2018-07-05 19:09:57 +0900

    UPDATE : Add file delete codes

--- py_convert_image/main.py
+++ py_convert_image/main.py
@@ -2,10 +2,14 @@ import py_convert_image
 import py_getImage_info
 import py_fileIO_list
 import arduino
+import os, sys
 
 #   Get File Names
 fileName = input('INPUT FILE NAME >> ')
 convert_image_name = input('INPUT CONVERT IMAGE NAME >> ')
+
+makedir = "mkdir /home/pi/rpiProject-PixelPicture/ino/%s" % fileName
+os.system(makedir)
 
 def start(_fileName, _convert_image_name):
 
@@ -17,3 +21,9 @@ list = py_fileIO_list._fileIO_list(fileName, start(fileName, convert_image_name)
 print(list)
 
 arduino.arduino_make_code(fileName, list)
+
+os.system("/home/pi/arduino-1.8.5/arduino --board arduino:avr:mega:cpu=atmega2560 --port /dev/ttyACM0 --upload /home/pi/rpiProject-PixelPicture/ino/%s/%s.ino"%(fileName, fileName))
+
+print("Successful UPLOAD!!")
+
+os.system("rm -r /home/pi/rpiProject-PixelPicture/converted_image/%s /home/pi/rpiProject-PixelPicture/ino/%s /home/pi/rpiProject-PixelPicture/list/%s.txt"%(convert_image_name, fileName, fileName))
